b3dfad094a6cd72e1b360424c8033aabe97cbded
“JyothiThipplikat” <lilmuthu4life21@gmail.com>
2018-09-26 23:37:12 -0500

    controller

--- app/controllers/client/products_controller.rb
+++ app/controllers/client/products_controller.rb
@@ -1,5 +1,5 @@
 class Client::ProductsController < ApplicationController
-   def index
+  def index
     response = HTTP.get("http://localhost:3000/api/products")
     @products = response.parse
     render "index.html.erb"
